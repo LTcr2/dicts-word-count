73f02a9d22ad4cf6f13cb495bedd4067d4b85234
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-15 12:48:28 -0700

    Additional refactoring with comments

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,7 @@
 # put your code here.
 def count_words(filename):
 
-    # open_file = open(filename)
+    # open_file = open(filename)(Ashley explained that the concept of "with")
     word_counts = {}
 
     with open(filename) as open_file:
@@ -11,8 +11,8 @@ def count_words(filename):
             for word in line:
                 word_counts[word] = word_counts.get(word, 0)+1
 
-    for keys in word_counts:
-        print(f"{keys} {word_counts[keys]}")
-    # open_file.close()
+    for keys, number in word_counts.items():
+        print(f"{keys} {number}") #refactored to implement the .items function
+    # open_file.close() (Removed duirng Ashley's explanation)
 
-count_words("twain.txt")
+count_words("test.txt")
