5f091c855455f1a187ad61f8b54d7924f4879664
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-15 12:28:50 -0700

    Completed exercise. Includes refactoring from code review.

--- wordcount.py
+++ wordcount.py
@@ -1,16 +1,18 @@
 # put your code here.
 def count_words(filename):
 
-    open_file = open(filename)
+    # open_file = open(filename)
     word_counts = {}
 
-
-    for line in open_file:
-        line = line.split(" ")
-        for word in line:
-            word_counts[word] = word_counts.get(word, 0)+1
+    with open(filename) as open_file:
+        for line in open_file:
+            line = line.rstrip()
+            line = line.split(" ")
+            for word in line:
+                word_counts[word] = word_counts.get(word, 0)+1
 
     for keys in word_counts:
         print(f"{keys} {word_counts[keys]}")
+    # open_file.close()
 
-count_words("test.txt")
+count_words("twain.txt")
